e820724676799bac6934017a20c87086f8796c7c
noripi10 <sgym.snk@gmail.com>
2022-08-20 22:19:53 +0900

    first commit

---
+++ graph/resolver.go
@@ -0,0 +1,10 @@
+package graph
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+import "github.com/noripi10/graphql-practice-app/graph/model"
+
+type Resolver struct{
+	todos []*model.Todo
+}
---
+++ server.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/noripi10/graphql-practice-app/graph"
+	"github.com/noripi10/graphql-practice-app/graph/generated"
+	"github.com/rs/cors"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+	c := cors.New(cors.Options{
+		AllowedOrigins: []string{"http://localhost:3000"},
+		AllowCredentials: true,
+	})
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", c.Handler(srv))
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
